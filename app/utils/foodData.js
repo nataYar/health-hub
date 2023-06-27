ceb59073b5a9f3a9359cf6f40953b735d0fba27d
nataliaYarr <n.yarysheva@gmail.com>
2023-06-26 22:40:56 -0700

    made nutrition api  url conditional

--- app/utils/foodData.js
+++ app/utils/foodData.js
@@ -4,47 +4,46 @@ const foodApiKey = process.env.NEXT_PUBLIC_EDAMAM_FOOD_KEY;
 const nutritionApiId = process.env.NEXT_PUBLIC_EDAMAM_NUTRITION_ID;
 const nutritionApiKey = process.env.NEXT_PUBLIC_EDAMAM_NUTRITION_KEY;
 
-const recipeApiId = process.env.NEXT_PUBLIC_EDAMAM_RECIPE_ID
-const recipeApiKey = process.env.NEXT_PUBLIC_EDAMAM_RECIPE_KEY
+const recipeApiId = process.env.NEXT_PUBLIC_EDAMAM_RECIPE_ID;
+const recipeApiKey = process.env.NEXT_PUBLIC_EDAMAM_RECIPE_KEY;
 export const fetchFoodData = async (searchQuery) => {
-   
-    try {
-      const response = await fetch(
-        `https://api.edamam.com/api/food-database/v2/parser?app_id=${foodApiId}&app_key=${foodApiKey}&ingr=${searchQuery}&nutrition-type=cooking`
-      );
-  
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-  
-      const data = await response.json();
-      return data
-    } catch (error) {
-      console.error(error);
+  try {
+    const response = await fetch(
+      `https://api.edamam.com/api/food-database/v2/parser?app_id=${foodApiId}&app_key=${foodApiKey}&ingr=${searchQuery}&nutrition-type=cooking`
+    );
+
+    if (!response.ok) {
+      throw new Error("Request failed");
     }
-  };
 
-  export const fetchNutritionData = async (searchQuery) => {
-   
-    try {
-      const response = await fetch(
-        `https://api.edamam.com/api/nutrition-details?app_id=${nutritionApiId}&app_key=${nutritionApiKey}`
-        `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${nutritionApiId}&app_key=${nutritionApiKey}&diet=low-fat&health=low-sugar`
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-      );
-  
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-  
-      const data = await response.json();
-      return data
-    } catch (error) {
-      console.error(error);
+export const fetchNutritionData = async (searchQuery, diet = "") => {
+  try {
+    let apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${nutritionApiId}&app_key=${nutritionApiKey}`;
+
+    if (diet) {
+      apiUrl += `&diet=${diet}`;
     }
-  };
 
-  // https://api.edamam.com/api/nutrition-details?app_id=a8451563&app_key=c17250c5ea86c79ed53cba21d1daa96e //nutrition
+    const response = await fetch(apiUrl);
 
+    if (!response.ok) {
+      throw new Error("Request failed");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// https://api.edamam.com/api/nutrition-details?app_id=a8451563&app_key=c17250c5ea86c79ed53cba21d1daa96e //nutrition
 
 //   https://api.edamam.com/api/food-database/v2/parser?app_id=8e0bd067&app_key=6a38cf87ae8017de073395be73379658&ingr=banana&nutrition-type=cooking
