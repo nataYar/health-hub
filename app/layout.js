93c7c41c00fe8b8bf0adea4d7420d6ecb12f9e90
nataliaYarr <n.yarysheva@gmail.com>
2023-07-29 00:15:19 -0700

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+      <link rel='icon' href='/favicon.ico'/>
+      </head>
       <body style={{ overflow: "visible" }}>
         <Providers>
        
